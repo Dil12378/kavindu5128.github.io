d9aff28bbc30c2a639675bcfd7c935c1e64e6c2d
kavindu5128 <120564830+kavindu5128@users.noreply.github.com>
2022-12-27 19:04:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
 <?php
-    $ch=curl_init();
-
-    curl_setopt($ch,CURLOPT_URL,"https://www.google.com");
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-    curl_setopt($ch,CURLOPT_HEADER,0);
-
-    $output=curl_exec($ch);
-
-    if($output===FALSE){
-        echo"my error".curl_error($ch);
-    }
-
-    curl_close($ch);
-
-    print_r($output);
+    
  ?>
